a5dcfb91b2ddb221ae896ea9adc7c9fe8ecf6d88
vadim <gabderahmanov99@gmail.com>
2017-07-17 12:35:37 +0400

    add TranslationONS method

--- src/ru/vad/binarycalculator/BinaryCalculator.java
+++ src/ru/vad/binarycalculator/BinaryCalculator.java
@@ -1,5 +1,31 @@
 package ru.vad.binarycalculator;
 
+import java.util.ArrayList;
+
 public class BinaryCalculator {
 	
+	
+	//Method translation from the binary system in the decimal
+	public int TranslationONS(int a) {
+		
+
+		// Subsidiary array
+		int[] sub = new int[a];
+		ArrayList<Integer> count = new ArrayList<>();
+		
+		for (int i = 0; i < sub.length; i++) {
+			if (a % 2 == 0) {
+				count.add(0);
+			} else {
+				count.add(1);
+			}
+		}		
+		return a;
+	}
+	
+	
+
+	public void accountAmount(int a, int b) {
+
+	}
 }
