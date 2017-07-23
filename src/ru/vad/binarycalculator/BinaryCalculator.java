81f2d8a2d3d430add666e4d8a9749027498c38b4
vadim <gabderahmanov99@gmail.com>
2017-07-23 11:06:55 +0400

    add method accountMultip

--- src/ru/vad/binarycalculator/BinaryCalculator.java
+++ src/ru/vad/binarycalculator/BinaryCalculator.java
@@ -28,6 +28,7 @@ public class BinaryCalculator {
 
 	}
 
+	//method summ two numbers
 	public String accountAmount(int a, int b) {
 		String str="";
 		StringBuffer sBuf = new StringBuffer(str);
@@ -45,6 +46,28 @@ public class BinaryCalculator {
 				str = str+"1";
 			}else if(af.startsWith("1", i)&&bf.startsWith("1", i)){
 				sBuf.insert(i-1, '1');
+			}						
+		}				
+		return sBuf.toString();
+	}
+	//Multiplication of numbers
+	public String accountMultip(int a, int b) {
+		String str="";
+		StringBuffer sBuf = new StringBuffer(str);
+		
+		String af = TranslationONS(a);
+		String bf = TranslationONS(b);
+		
+		int omp = af.length()+(bf.length()-af.length());
+		
+		for (int i =0;i<omp;i++){
+			
+			if(af.startsWith("0", i)&&bf.startsWith("0", i)){
+				str = str+"0";				
+			}else if(af.startsWith("1", i)&&bf.startsWith("0", i)||af.startsWith("0", i)&&bf.startsWith("1", i)){
+				str = str+"0";
+			}else if(af.startsWith("1", i)&&bf.startsWith("1", i)){
+				str = str+"1";
 			}						
 		}				
 		return sBuf.toString();
--- src/ru/vad/binarycalculator/Run.java
+++ src/ru/vad/binarycalculator/Run.java
@@ -21,7 +21,7 @@ public class Run {
 	public static void main(String[] args) {
 		BinaryCalculator bc = new BinaryCalculator();
 
-		System.out.println(bc.TranslationONS(255));		
+		System.out.println(bc.TranslationONS(255));
 
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
@@ -36,71 +36,72 @@ public class Run {
 		JTextField ftr2 = new JTextField(5);
 		JTextField result = new JTextField(10);
 		Choice cho = new Choice();
-		
+
 		ftr.setText("0");
 		ftr2.setText("0");
-		
-		txt1.setText("0");
-		txt2.setText("0");
 
 		cho.add("+");
 		cho.add("-");
 		cho.add("*");
-		cho.add("/");				
-
-		if (cho.getSelectedIndex() == 0) {
-			label1.setText("+");
-
-		} else if (cho.getSelectedIndex() == 1) {
-			label1.setText("-");
-
-		} else if (cho.getSelectedIndex() == 1) {
-			label1.setText("*");
-
-		} else if (cho.getSelectedIndex() == 1) {
-
-			label1.setText("/");
-		}
+		cho.add("/");	
 
 		translate.add(ftr);
 		translate.add(forTranslate);
 		translate.add(ftr2);
-		
+
 		button1.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				int a = Integer.getInteger(txt1.getText());
-				int b = Integer.getInteger(txt2.getText());
-				result.setText(bc.accountAmount(a, b));
-				
+				int a;
+				int b;
+				a = Integer.getInteger(txt1.getText());
+				b = Integer.getInteger(txt2.getText());
+
+				if (cho.getSelectedIndex() == 0) {
+					label1.setText("+");
+					result.setText(bc.accountAmount(a, b));
+
+				} else if (cho.getSelectedIndex() == 1) {
+					label1.setText("-");
+
+				} else if (cho.getSelectedIndex() == 2) {
+					label1.setText("*");
+					result.setText(bc.accountMultip(a, b));
+
+				} else if (cho.getSelectedIndex() == 3) {
+
+					label1.setText("/");
+				}
+
 			}
 		});
 
 		button2.addActionListener(new ActionListener() {
 
 			@Override
-			public void actionPerformed(ActionEvent e) {				
+			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(null, translate, "Translation",JOptionPane.PLAIN_MESSAGE);
-				
+
 			}
 		});
-		
+
 		forTranslate.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				int a = Integer.parseInt(ftr.getText());
 				ftr2.setText(bc.TranslationONS(a));
-				
+
 			}
 		});
 
-		panel.add(button2);
+		panel.add(cho);
+		panel.add(button1);
 		panel.add(txt1);
 		panel.add(label1);
 		panel.add(txt2);
-		panel.add(button1);
+		panel.add(button2);
 		panel.add(result);
 
 		frame.add(panel);
