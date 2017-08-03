4313140e38c664db9ae2774a9e353ad8f096fcc6
vadim <gabderahmanov99@gmail.com>
2017-08-03 11:41:17 +0400

    add buttonlistener class

---
+++ src/ru/vad/binarycalculator/ButtonListener.java
@@ -0,0 +1,13 @@
+package ru.vad.binarycalculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+				
+	}
+	
+}
