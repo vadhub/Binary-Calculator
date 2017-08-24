9733018500cfd64b362309f0cfba721e7f13115f
vadim <gabderahmanov99@gmail.com>
2017-08-24 09:03:51 +0400

    add ConvertLettersToNumbers class

---
+++ src/ru/vad/binarycalculator/ConvertLettersToNumbers.java
@@ -0,0 +1,7 @@
+package ru.vad.binarycalculator;
+
+public class ConvertLettersToNumbers {
+	String[] lettersUpperRegisters = {"A","B","C","D","E","F"};
+	String[] letterslowerRegisters = {"a","b","c","d","e","f"};	
+	
+}
