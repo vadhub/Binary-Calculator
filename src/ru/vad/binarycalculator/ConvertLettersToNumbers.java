a2900193bd19bf546d9208f735b6180cfa171cf3
vadim <gabderahmanov99@gmail.com>
2017-08-24 09:08:46 +0400

    add convert method

--- src/ru/vad/binarycalculator/ConvertLettersToNumbers.java
+++ src/ru/vad/binarycalculator/ConvertLettersToNumbers.java
@@ -2,6 +2,13 @@ package ru.vad.binarycalculator;
 
 public class ConvertLettersToNumbers {
 	String[] lettersUpperRegisters = {"A","B","C","D","E","F"};
-	String[] letterslowerRegisters = {"a","b","c","d","e","f"};	
+	String[] lettersLowerRegisters = {"a","b","c","d","e","f"};
+	
+	public void convert(String let){
+		for(int i = 0;i<lettersUpperRegisters.length;i++)
+		if(let.equals(lettersUpperRegisters[i])||let.equals(lettersLowerRegisters[i])){
+									
+		}
+	}
 	
 }
