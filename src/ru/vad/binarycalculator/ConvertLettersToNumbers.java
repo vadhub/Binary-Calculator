f114416c2f41849d5f47e5f8df0e372f7d883db1
vadim <gabderahmanov99@gmail.com>
2017-08-24 09:14:54 +0400

    change ConvertLettersToNumbers class

--- src/ru/vad/binarycalculator/ConvertLettersToNumbers.java
+++ src/ru/vad/binarycalculator/ConvertLettersToNumbers.java
@@ -4,11 +4,14 @@ public class ConvertLettersToNumbers {
 	String[] lettersUpperRegisters = {"A","B","C","D","E","F"};
 	String[] lettersLowerRegisters = {"a","b","c","d","e","f"};
 	
-	public void convert(String let){
+	String[] ConvertedNumbers = {"1010","1011","1100","1101","1110","1111"};
+	
+	public String convert(String let){
 		for(int i = 0;i<lettersUpperRegisters.length;i++)
 		if(let.equals(lettersUpperRegisters[i])||let.equals(lettersLowerRegisters[i])){
-									
+			return ConvertedNumbers[i];							
 		}
+		return null;
 	}
 	
 }
